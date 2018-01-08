eb59da5f7cf5647ca457c324f3d584ccbd8921b1
malhere_bastien <malherebastien76@hotmail.com>
2018-01-08 08:57:34 +0100

    First commit

---
+++ src/FormulaireChaine.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class FormulaireChaine extends JFrame
+{
+    private String label;
+    private String id;
+    private int x;
+    private int y;
+    private int largeur;
+    private String type;
+
+    public FormulaireChaine(String label, String id, int x, int y, int largeur, String type)
+    {
+        this.label = label;
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.largeur = largeur;
+        this.type = type;
+
+
+    }
+
+
+}
