27173d85d52e1599bf8767e25756c4eb52062c81
ninatella76 <ninatella76@gmail.com>
2018-01-12 11:24:31 +0100

    MAJ Calendrier.java 30 et 31 gérer pour la demi année

--- src/Calendrier.java
+++ src/Calendrier.java
@@ -73,8 +73,10 @@ public class Calendrier extends JFrame implements ActionListener, ItemListener,
 
 	private void remplirJour() {
 		viderCache();
-		if (cbMois.getSelectedIndex() % 2 == 0) {
-			for (int i = 1; i <= 30; i++) {
+		if (((cbMois.getSelectedIndex() < 7) && cbMois.getSelectedIndex() % 2 == 0)
+				|| (cbMois.getSelectedIndex() >= 7)
+				&& cbMois.getSelectedIndex() % 2 != 0) {
+			for (int i = 1; i <= 31; i++) {
 				tabButton.add(new JButton("" + i));
 				tabButton.get(i - 1).addActionListener(this);
 				panelJour.add(tabButton.get(i - 1));
@@ -95,7 +97,7 @@ public class Calendrier extends JFrame implements ActionListener, ItemListener,
 
 					}
 			} else {
-				for (int i = 1; i <= 31; i++) {
+				for (int i = 1; i <= 30; i++) {
 					tabButton.add(new JButton("" + i));
 					tabButton.get(i - 1).addActionListener(this);
 					panelJour.add(tabButton.get(i - 1));
